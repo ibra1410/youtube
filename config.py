5ecda520bf2b256458a66cc026c127ad535e6109
ibra1410 <160061682+ibra1410@users.noreply.github.com>
2024-06-09 19:33:43 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+API_ID = int(os.getenv("API_ID", "21095119"))  # ضيف هنا ال اي بي ايدي
+API_HASH = os.getenv("API_HASH", "74a3ccfca476db8f3bde201bec15ee6c")  # ضيف هنا الاي بي هاش
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7221639114:AAG2o9vWXrs-4q11eF4cIRGn-pcMwbNzurA")  # حط هنا توكن بوتك
